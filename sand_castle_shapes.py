ada22efd732991395cabd1e626e103ddf434459f
wkostuch <wkostuch@gmail.com>
2020-03-07 11:02:27 -0600

    Add Cone shape + methods

--- sand_castle_shapes.py
+++ sand_castle_shapes.py
@@ -143,7 +143,7 @@ PYRAMID
 class Pyramid:
     side_length: float
     height: float
-    angle: float #angle from vertical that the pyramid faces are leaning towards each other on, in radians
+    angle: float #internal angle of the pyramid (two base angles of a cut-away pyramid slice)
     sand_grain_number: float
     base_radius: float
     base_side_length: float
@@ -160,6 +160,7 @@ class Pyramid:
         self.height = height
         self.base_radius = Pyramid.determine_square_radius(self, side)
         self.base_side_length = side
+        self.angle = math.atan(height / (side / 2))
     
     #sets the base_height value
     def set_base_height(self, h: float) -> float:
@@ -188,7 +189,7 @@ class Pyramid:
         #Compute volume by getting volume of the rectangular brick and then adding the "ramps" 
         # on each side of the base
         #Get the theta value for the angle in the triangle we're considering
-        theta =  (math.pi / 2) - self.angle
+        theta =  self.angle
         #Make opposite, adjacent, and hypotenuse values
         #See pics in git repo of big_board for the diagram on these values
         opp = self.base_height 
@@ -227,4 +228,84 @@ class Pyramid:
 '''
 CONE
 '''
+
+class Cone:
+    radius: float
+    height: float
+    angle: float #internal angle of the cylinder in radians
+    sand_grain_number: float
+    base_height_ratio: float #used for getting r at a height for the truncated cone
+    base_radius: float
+    base_height: float
+    radius_above_base_height: float #radius of the Cone just above the erosion
+    #NOTE: base_radius field is the radius we're using when determining when the sandcastle will fall;
+    #      that is, it's the R that is being eroded
+    #      base_height is the height of the Cube that is being hit by the wave (same as wave height)
+
+
+    #Constructor for Cones
+    def __init__(self, r: float, h: float):
+        self.radius = r
+        self.height = h
+        self.base_radius = r
+        self.height_radius_ratio = h / r
+        self.angle = math.arctan(h / r)
+        
+
+    #sets the base_height value, and the radius_above_base_height value as well
+    def set_base_height(self, h: float) -> float:
+        self.base_height = h
+        self.radius_above_base_height = self.get_radius_at_h(self.radius, h)
+
+    #Returns the radius value at base_height (so: radius at the top of the wave-erosion area)
+    def get_radius_at_base_height(self) -> float:
+        #h is the height of the imaginary cone extending from the truncated cone that is the 
+        # base being eroded; use the base_height_ratio to keep it consistent
+        h = self.height_radius_ratio * self.base_radius
+        r = get_radius_at_h(self.base_radius, h)
+        return r
+
+    #returns the radius of the Cone at height h
+    #NOTE that base_radius is not self here, it varies 
+    def get_radius_at_h(self, base_radius: float, h: float) -> float:
+        triangle_adj = h / math.tan(self.angle)
+        return base_radius - triangle_adj
+
+
+    #Returns the area of the base of the Cone that doesn't get eroded
+    def get_top_area(self) -> float:
+        top = math.pi * self.radius_above_base_height * self.radius_above_base_height
+        return top
+
+    #Returns the area of the of the base of the Cone, even during erosion process
+    def get_eroded_base(self) -> float:
+        base = math.pi * self.base_radius * self.base_radius
+
+    #Returns the volume of the Cone as it currently stands
+    def get_volume(self) -> float:
+        top_vol = self.get_top_vol()
+        bottom_vol = self.get_eroded_vol()
+        return top_vol + bottom_vol
+
+    #Returns the volume of the top part of the Cone that never gets eroded by a wave
+    def get_top_vol(self) -> float:
+        return ((self.height - self.base_height) * self.get_top_area() ) / 3
+
+    #Returns the volume of the bottom part of the Cone that does get hit by waves
+    def get_eroded_vol(self) -> float:
+        #NOTE: find formula for truncated cone volumes
+        return self.base_height * self.get_eroded_base()
+
+    #updates the base_radius
+    def update_base_radius(self, n: float):
+        self.base_radius = n
+
+    #Returns the force of the top of the Cylinder weighing down on the eroding base
+    def get_normal_sand(self) -> float:
+        global SAND_DENSITY
+        global GRAVITY
+        top_vol = self.get_top_vol()
+        weight = top_vol * SAND_DENSITY * GRAVITY
+        return weight
+
 
