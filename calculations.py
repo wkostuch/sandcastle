5787ac3c230ca8239fb4837b15d055d62833121d
Cameron Nottingham <cjnott1999@gmail.com>
2020-03-07 20:32:26 -0600

    Added function to determine max shear strength

--- calculations.py
+++ calculations.py
@@ -1,32 +1,39 @@
 import math
 import sand_castle_shapes
+import wave
 
 
 #returns max shear strength given a shape, wave height, and bond number
-
-def maximum_shear_strength(shape, wave_height, z) -> float:
+def maximum_shear_strength(shape, wave, z) -> float:
 
     #set the height based on this particular wave
-    shape.set_base_height(wave_height)
+    shape.set_base_height(wave.wave_height)
 
-    
     #Set the needed variables for the function
-
     #Normal stress
     normal_force = shape.get_normal_sand()
-    area = get_cross_sectional_area()
+    area = shape.get_cross_sectional_area()
     mu = 0.66
+    
 
-    #Cohesion 
+    #Calculate the shear strength
+    shear_strength = ((normal_force / area) *  mu) + cohesion(z)
+
+    return shear_strength
+
+#Determin the cohesion for a given bond number z 
+def cohesion(z) -> float:
+    
     volume_correction = (3 / (4 * math.pi))
     phi = 0.6 
     kappa = 0.4
     s = 0.5
+    sand_size = 0.000375
+    mu = 0.66
 
-    #Calculate the shear strength
-    shear_strength = ((normal_force / area) *  mu) + ((volume_correction) * s * mu * ((phi * kappa * z) / sand_density))
+    cohesion = ((volume_correction) * s * mu * ((phi * kappa * z) / sand_size))
+    return cohesion
 
-    return shear_strength
 
 
 
